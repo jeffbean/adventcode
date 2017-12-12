4b2f438e7e2cef3772aa3143310d77fffcdbc1a1
Jeffrey Bean <jeffreyrobertbean@gmail.com>
2017-12-11 20:21:48 -0800

    adding helper to speed up file read

--- 2017/common/file.go
+++ 2017/common/file.go
@@ -3,7 +3,11 @@ package common
 import (
 	"io/ioutil"
 	"log"
+	"path/filepath"
+	"runtime"
 )
+
+const _inputFileName = "input.txt"
 
 func check(e error) {
 	if e != nil {
@@ -16,4 +20,18 @@ func ReadFile(path string) []byte {
 	data, err := ioutil.ReadFile(path)
 	check(err)
 	return data
+}
+
+// GetInputFile reads in the eecutables input.txt file
+func GetInputFile() []byte {
+	_, filename, _, ok := runtime.Caller(1)
+	if !ok {
+		log.Fatal("caller filename not found")
+	}
+	dir, err := filepath.Abs(filepath.Dir(filename))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return ReadFile(filepath.Join(dir, _inputFileName))
 }
