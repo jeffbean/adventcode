a6d97e7e32287884704c1dae5e1e5d410bd21a1f
Jeffrey Bean <jeffreyrobertbean@gmail.com>
2017-12-11 20:22:09 -0800

    Adding template files to just copy from

---
+++ 2017/day00/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jeffbean/adventcode/2017/common"
+)
+
+func part1() int {
+	return 0
+}
+
+func part2() int {
+	return 0
+}
+
+func main() {
+	data := common.GetInputFile()
+	log.Println(data)
+	fmt.Printf("%v\n", part1())
+	fmt.Printf("%v\n", part2())
+}
