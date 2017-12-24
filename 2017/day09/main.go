183a72d716b9e5242930409cbb8a803ee67e7920
Jeffrey Bean <jeffreyrobertbean@gmail.com>
2017-12-24 10:08:19 -0800

    day 9 part 1 and 2

--- 2017/day09/main.go
+++ 2017/day09/main.go
@@ -34,13 +34,35 @@ func part1(data []byte) int {
 	return totalScore
 }
 
-func part2() int {
-	return 0
+func part2(data []byte) int {
+	totalScore := 0
+	garbage := false
+	for i := 0; i < len(data); i++ {
+		r := data[i]
+		switch r {
+		case 33:
+			i++
+			continue
+		case 60:
+			if garbage {
+				totalScore++
+			}
+			garbage = true
+		case 62:
+			garbage = false
+		default:
+			if garbage {
+				totalScore++
+			}
+		}
+		// fmt.Printf("pos %v rune: %#U groupScore %v\n", i, r, groupScore)
+	}
+	return totalScore
 }
 
 func main() {
 	data := common.GetInputFile()
 	// log.Println(data)
 	fmt.Printf("%v\n", part1(data))
-	fmt.Printf("%v\n", part2())
+	fmt.Printf("%v\n", part2(data))
 }
--- 2017/day09/main_test.go
+++ 2017/day09/main_test.go
@@ -7,15 +7,25 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	// assert.Equal(t, 6, part1([]byte("{{{}}}")))
-	// assert.Equal(t, 16, part1([]byte("{{{},{},{{}}}}")))
-	// assert.Equal(t, 1, part1([]byte("{<a>,<a>,<a>,<a>}")))
-	// assert.Equal(t, 9, part1([]byte("{{<ab>},{<ab>},{<ab>},{<ab>}}")))
+	assert.Equal(t, 6, part1([]byte("{{{}}}")))
+	assert.Equal(t, 16, part1([]byte("{{{},{},{{}}}}")))
+	assert.Equal(t, 1, part1([]byte("{<a>,<a>,<a>,<a>}")))
+	assert.Equal(t, 9, part1([]byte("{{<ab>},{<ab>},{<ab>},{<ab>}}")))
 
-	// assert.Equal(t, 9, part1([]byte("{{<!!>},{<!!>},{<!!>},{<!!>}}")))
+	assert.Equal(t, 9, part1([]byte("{{<!!>},{<!!>},{<!!>},{<!!>}}")))
 	assert.Equal(t, 3, part1([]byte("{{<a!>},{<a!>},{<a!>},{<ab>}}")), "{{<a!>},{<a!>},{<a!>},{<ab>}}")
 }
 
 func TestPart2(t *testing.T) {
+	assert.Equal(t, 0, part2([]byte("{{{}}}")))
+	assert.Equal(t, 0, part2([]byte("{{{},{},{{}}}}")))
+	assert.Equal(t, 4, part2([]byte("{<a>,<a>,<a>,<a>}")))
+	assert.Equal(t, 8, part2([]byte("{{<ab>},{<ab>},{<ab>},{<ab>}}")))
+
+	assert.Equal(t, 0, part2([]byte("{{<!!>},{<!!>},{<!!>},{<!!>}}")))
+	assert.Equal(t, 17, part2([]byte("{{<a!>},{<a!>},{<a!>},{<ab>}}")), "{{<a!>},{<a!>},{<a!>},{<ab>}}")
+	assert.Equal(t, 0, part2([]byte("<>")))
+	assert.Equal(t, 10, part2([]byte("<{o\"i!a,<{i<a>")))
+	assert.Equal(t, 2, part2([]byte("<{!>}>")))
 
 }
